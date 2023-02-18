4b66444072bfa3f834de0e89ffa06643cc367a6b
Viktor Kindrat <kindratvictor07@gmail.com>
2023-02-18 10:50:14 +0200

    update

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,26 +1,12 @@
 let emailform = document.forms.sender
 
-$("#sendBtn").click(function() {
-    let validity = emailform.checkValidity()
-    if (!validity) {
-        console.log("no valid")
-        return false
-    }
-
-    let elements = emailform.adressat;
-    let emails = [];
-
-    elements.forEach(element => {
-        emails.push(element.value)
-    });
-    console.log(emails)
-
-    fetch(`${location.origin}/send`, {
+function postServer(request, message) {
+    fetch(`${location.origin}/${request}`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify(emails)
+            body: message
         })
         .then(response => {
             return response.json();
@@ -33,5 +19,26 @@ $("#sendBtn").click(function() {
             alert(error)
             console.error('Error:', error);
         });
+}
 
+$("#sendBtn").click(function() {
+    let validity = emailform.checkValidity()
+    if (!validity) {
+        alert("Not send. Enter valid data")
+        return false
+    }
+
+    let elements = Array.from(emailform.adressat);
+    let emails = [];
+    if (elements.length > 0) {
+        elements.forEach(element => {
+            emails.push(element.value)
+        });
+    } else {
+        elements = emailform.adressat;
+        emails.push(elements.value)
+    }
+
+    postServer("send", JSON.stringify(emails))
+    $("#form__input-container").html(emailInputExample)
 })
